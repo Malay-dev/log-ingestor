9f12ee3fab30272cf4c4b94d962c88c6851cfb1a
Malay-dev <malaykumar2003@gmail.com>
2023-11-18 14:48:05 +0530

    Feat:

--- Controllers/log_controller.js
+++ Controllers/log_controller.js
@@ -19,7 +19,6 @@ const get_all_logs = async (req, res, next) => {
 const create_log = async (req, res, next) => {
   try {
     const log_data = req?.body;
-    console.log(req);
     const log = await LogSchema.create(log_data);
     res.status(200).json({
       status: "success",
--- index.js
+++ index.js
@@ -9,12 +9,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const MONGO_USER = process.env.MONGO_USER;
+const MONGO_USER = process.env.MONGO_USER || "mongodb-cfg";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_IP = process.env.MONGO_IP || "mongo-database";
+const MONGO_IP = process.env.MONGO_IP || "mongodb-sharded";
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 
-const DATABASE_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const DATABASE_URL = `mongodb://root:password123@${MONGO_IP}:${MONGO_PORT}/?authSource=admin `;
 
 const connectWithRetry = () => {
   mongoose
